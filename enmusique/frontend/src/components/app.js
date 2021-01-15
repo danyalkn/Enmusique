38bf4d1474c194c707a5a23961477f0f96415c71
danyal0726 <danyal0726@gmail.com>
2021-01-15 17:56:35 -0500

    Day 4  - Updating App.js

--- enmusique/frontend/src/components/app.js
+++ enmusique/frontend/src/components/app.js
@@ -1,14 +1,14 @@
-import React, {Component } from "react";
+import React, { Component } from "react";
 import { render } from "react-dom";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (<h1>Testing React Code</h1>)
-    }
+  render() {
+    return <h1>Hello</h1>
+  }
 }
 
 const appDiv = document.getElementById("app");
